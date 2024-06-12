ede17e3ad82ae3d52d4436410e774c1b3b5f0b9e
Shankyness <156609806+Shankyness@users.noreply.github.com>
2024-06-12 17:28:34 +0000

    2nd day

---
+++ basics/strings.js
@@ -0,0 +1,9 @@
+const gamename = new String('hiteshic')
+
+console.log(gamename[0]);
+
+console.log(gamename.__proto__);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+
---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accountID = 14453
+let accountEmail = "hitesh@gooogle.com"
+var accountpassword = "12345"
+accountCity = "jaipur"
+// accountID = 25 // not allowed
+
+console.log(accountID);
+
+console.table([accountID,accountEmail,accountpassword,accountCity])
